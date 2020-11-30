80ccaca00265f70617da2418ba918ef30105885b
hamada456 <n87.takaya.hamada@gmail.com>
2020-11-30 21:46:10 +0900

    修正２

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   def index
-    @items = Item.order(@item)
+    @items = Item.order(id: "DESC")
   end
 
   def new
