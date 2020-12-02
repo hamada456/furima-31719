b9e9e9a865020cbb6e4285b41acff95b0025fb9d
hamada456 <n87.takaya.hamada@gmail.com>
2020-12-02 19:31:20 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,17 +20,17 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
+    
   end
 
   def edit
-    @item = Item.find(params[:id])
+    
   end
 
   def update
-    @item = Item.find(params[:id])
+    
     @item.update(item_params)
-    if @item.save
+    if @item.valid?
       redirect_to root_path
     else
       render :new
