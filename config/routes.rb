882e65a3d369dc9e00cf89481128be0b9aa77309
hamada456 <n87.takaya.hamada@gmail.com>
2020-12-03 02:03:56 +0900

    修正

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   #root to: 'articles#new'
   resources :articles
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :items, only: [:index, :new, :create, :show, :edit, :update, :destroy]
+  resources :items
 end
